203913297f70996df95aa40afb38487d069595c4
Valencia052000 <valencia052000@mail.ru>
2024-12-15 01:41:53 +0300

    probuem

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import { initYandexMetrika } from 'yandex-metrika-vue3'
 
 // Components
 import App from './App.vue'
@@ -16,5 +17,10 @@ import { createApp } from 'vue'
 const app = createApp(App)
 
 registerPlugins(app)
+
+app.use(initYandexMetrika, {
+  id: 99215475,
+  env: import.meta.env.NODE_ENV,
+});
 
 app.mount('#app')
